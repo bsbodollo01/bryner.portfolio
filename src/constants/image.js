df888a26928f0867d64af3823eb93f1b1d93bb2a
Bryner Bodollo <bsbodollo01@gmail.com>
2023-08-03 19:03:42 +0800

    Feat: revise porfolio

--- src/constants/image.js
+++ src/constants/image.js
@@ -10,38 +10,48 @@ import tailwindLogo from "../assets/logo/tailwind-logo.svg";
 import gitLogo from "../assets/logo/git-logo.svg";
 import githubLogo from "../assets/logo/github-logo.svg";
 import nextLogo from "../assets/logo/next-logo.svg";
-import hubspotLogo from "../assets/logo/hubspot-logo.svg";
+import svelteLogo from "../assets/logo/svelte-logo.svg";
+import bootstrapLogo from "../assets/logo/bootstrap-logo.svg";
+import reduxLogo from "../assets/logo/redux-logo.svg";
+import laravelLogo from "../assets/logo/laravel-logo.svg";
+import mysqlLogo from "../assets/logo/mysql-logo.svg";
 import brynerImage from "../assets/bryner.jpg";
-import bluecheckImage from "../assets/projects/bluecheck-visible.png";
-import catifyImage from "../assets/projects/catify-visible.png";
-import shortlyImage from "../assets/projects/shortly-visible.png";
-import sunnysideImage from "../assets/projects/sunnyside-visible.png";
+import brookyImage from "../assets/projects/brooky-portal.png";
+import dazleImage from "../assets/projects/dazle-portal.png";
+import dimtsImage from "../assets/projects/dimts-portal.png";
+import myemedImage from "../assets/projects/myemed-portal.png";
+import dictImage from "../assets/projects/dict-portal.png";
 import errorIllustration from "../assets/illustrations/404-vector.svg";
 import successIllustration from "../assets/illustrations/success-vector.svg";
 import brandLogoLight from "../assets/icons/logo-lighter.png";
 import brandLogoDark from "../assets/icons/logo-darker.png";
 
 export default {
-  emailIcon,
-  upworkIcon,
-  githubIcon,
-  linkedinIcon,
-  htmlLogo,
-  cssLogo,
-  javascriptLogo,
-  reactLogo,
-  tailwindLogo,
-  gitLogo,
-  githubLogo,
-  nextLogo,
-  hubspotLogo,
-  brynerImage,
-  bluecheckImage,
-  catifyImage,
-  shortlyImage,
-  sunnysideImage,
-  errorIllustration,
-  successIllustration,
-  brandLogoLight,
-  brandLogoDark,
+    emailIcon,
+    upworkIcon,
+    githubIcon,
+    linkedinIcon,
+    htmlLogo,
+    cssLogo,
+    javascriptLogo,
+    reactLogo,
+    tailwindLogo,
+    gitLogo,
+    githubLogo,
+    nextLogo,
+    svelteLogo,
+    bootstrapLogo,
+    reduxLogo,
+    laravelLogo,
+    mysqlLogo,
+    brynerImage,
+    brookyImage,
+    dazleImage,
+    dimtsImage,
+    myemedImage,
+    dictImage,
+    errorIllustration,
+    successIllustration,
+    brandLogoLight,
+    brandLogoDark,
 };
